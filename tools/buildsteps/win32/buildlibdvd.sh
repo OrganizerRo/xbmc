f8db0cc073a5e195cd9b5e85f65bec3ee01fb171
Jerome Fountain <jfountain@firstam.com>
2026-04-10 06:13:44 -0700

    Added variable overrides

--- tools/buildsteps/win32/buildlibdvd.sh
+++ tools/buildsteps/win32/buildlibdvd.sh
@@ -4,8 +4,11 @@
     source buildhelpers.sh
 
 LIBDVDPREFIX=/xbmc/lib/libdvd
-PKG_CONFIG_PATH=$LIBDVDPREFIX/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}
+PKG_CONFIG_PATH=$LIBDVDPREFIX/lib/pkgconfig:$LIBDVDPREFIX/share/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}
 export PKG_CONFIG_PATH
+
+# Ensure output directories exist before any install step writes to them.
+mkdir -p "$LIBDVDPREFIX/bin" "$LIBDVDPREFIX/lib/pkgconfig" "$LIBDVDPREFIX/include"
 
 do_load_autoconf() {
   do_loaddeps $1
@@ -16,9 +19,9 @@ do_load_autoconf() {
 
 #libdvdcss
 do_load_autoconf /xbmc/tools/depends/target/libdvdcss/DVDCSS-VERSION
-CC="gcc -static-libgcc" \
 ./configure \
       --prefix=$LIBDVDPREFIX \
+      CC="gcc -static-libgcc" \
       CFLAGS="-DNDEBUG" \
       --disable-doc \
       --enable-shared \
@@ -31,27 +34,27 @@ cp "$LIBDVDPREFIX/bin/libdvdcss-2.dll" /xbmc/system/
 
 #libdvdread
 do_load_autoconf /xbmc/tools/depends/target/libdvdread/DVDREAD-VERSION
-CC="gcc -static-libgcc" \
-CSS_CFLAGS="-I$LIBDVDPREFIX/include" \
-CSS_LIBS="-L$LIBDVDPREFIX/lib -ldvdcss" \
 ./configure \
     --prefix=$LIBDVDPREFIX \
    --disable-shared \
    --enable-static \
    --with-libdvdcss \
+   CC="gcc -static-libgcc" \
+   CSS_CFLAGS="-I$LIBDVDPREFIX/include" \
+   CSS_LIBS="-L$LIBDVDPREFIX/lib -ldvdcss" \
    CFLAGS="-DHAVE_DVDCSS_DVDCSS_H -D_XBMC -DNDEBUG -I$LIBDVDPREFIX/include" \
    --build="$MINGW_CHOST" || exit 1
 do_makelib $MAKEFLAGS || exit 1
 
 #libdvdnav
 do_load_autoconf /xbmc/tools/depends/target/libdvdnav/DVDNAV-VERSION
-CC="gcc -static-libgcc" \
-DVDREAD_CFLAGS="-I$LIBDVDPREFIX/include" \
-DVDREAD_LIBS="-L$LIBDVDPREFIX/lib -ldvdread" \
 ./configure \
    --prefix=$LIBDVDPREFIX \
    --disable-shared \
    --enable-static \
+   CC="gcc -static-libgcc" \
+   DVDREAD_CFLAGS="-I$LIBDVDPREFIX/include" \
+   DVDREAD_LIBS="-L$LIBDVDPREFIX/lib -ldvdread" \
    CFLAGS="-D_XBMC -DNDEBUG -I$LIBDVDPREFIX/include" \
    --build="$MINGW_CHOST" || exit 1
 do_makelib $MAKEFLAGS || exit 1
