fe62b96dd18f2f47f29b13b77bbdb24444b2b98a
Jerome Fountain <jfountain@firstam.com>
2026-04-10 00:52:59 -0700

    Fix to libDVDcss

--- tools/buildsteps/win32/buildlibdvd.sh
+++ tools/buildsteps/win32/buildlibdvd.sh
@@ -4,7 +4,7 @@
     source buildhelpers.sh
 
 LIBDVDPREFIX=/xbmc/lib/libdvd
-PKG_CONFIG_PATH=$LIBDVDPREFIX/lib/pkgconfig
+PKG_CONFIG_PATH=$LIBDVDPREFIX/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}
 export PKG_CONFIG_PATH
 
 do_load_autoconf() {
@@ -21,35 +21,40 @@ CC="gcc -static-libgcc" \
       --prefix=$LIBDVDPREFIX \
       CFLAGS="-DNDEBUG" \
       --disable-doc \
-      --with-pic \
-      --build="$MINGW_CHOST"
-do_makelib $MAKEFLAGS
+      --enable-shared \
+      --disable-static \
+      --build="$MINGW_CHOST" || exit 1
+do_makelib $MAKEFLAGS || exit 1
 
 strip -S $LIBDVDPREFIX/bin/libdvdcss-2.dll &&
 cp "$LIBDVDPREFIX/bin/libdvdcss-2.dll" /xbmc/system/
 
 #libdvdread
-do_load_autoconf /xbmc/tools/depends/target/libdvdread/DVDREAD-VERSION 
+do_load_autoconf /xbmc/tools/depends/target/libdvdread/DVDREAD-VERSION
 CC="gcc -static-libgcc" \
+CSS_CFLAGS="-I$LIBDVDPREFIX/include" \
+CSS_LIBS="-L$LIBDVDPREFIX/lib -ldvdcss" \
 ./configure \
     --prefix=$LIBDVDPREFIX \
    --disable-shared \
    --enable-static \
    --with-libdvdcss \
    CFLAGS="-DHAVE_DVDCSS_DVDCSS_H -D_XBMC -DNDEBUG -I$LIBDVDPREFIX/include" \
-   --build="$MINGW_CHOST"
-do_makelib $MAKEFLAGS
+   --build="$MINGW_CHOST" || exit 1
+do_makelib $MAKEFLAGS || exit 1
 
 #libdvdnav
-do_load_autoconf /xbmc/tools/depends/target/libdvdnav/DVDNAV-VERSION 
+do_load_autoconf /xbmc/tools/depends/target/libdvdnav/DVDNAV-VERSION
 CC="gcc -static-libgcc" \
+DVDREAD_CFLAGS="-I$LIBDVDPREFIX/include" \
+DVDREAD_LIBS="-L$LIBDVDPREFIX/lib -ldvdread" \
 ./configure \
    --prefix=$LIBDVDPREFIX \
    --disable-shared \
    --enable-static \
    CFLAGS="-D_XBMC -DNDEBUG -I$LIBDVDPREFIX/include" \
-   --build="$MINGW_CHOST"
-do_makelib $MAKEFLAGS
+   --build="$MINGW_CHOST" || exit 1
+do_makelib $MAKEFLAGS || exit 1
 
 cd $LOCALBUILDDIR
 gcc \
